a3ba227ff6c3d68d472d47460a75748ad6fe87f1
onesc <onescyt@gmail.com>
2016-06-01 16:36:05 +1000

    groups to submit image to displayed as a dropdown menu

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -10,13 +10,13 @@ class ImagesController < ApplicationController
       params[:image][:user_id] = @current_user.id
 
 
-      if params[:group_id] == nil
+      if params[:image][:group_id] == nil
+        raise "hell"
         flash[:error] = "You must choose a group to submit to"
         redirect_to new_image_path
       else
-        params[:image][:group_id] = params[:group_id][:group_id]
         @image = Image.create user_params
-        redirect_to home_path
+        redirect_to Group.find_by(id: params[:image][:group_id])
       end
   end
 
