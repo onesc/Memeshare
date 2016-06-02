77e20af73f5fb79ffe847a904ad93aaf5cab2445
onesc <onescyt@gmail.com>
2016-06-02 17:29:44 +1000

    home page now displays all images that user may view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,19 @@ class WelcomeController < ApplicationController
   def index
     @groups = Group.all
     @memb_assocs = UsersGroup.all
+
+    @images = []
+    @groups_belong_to = []
+
+      @groups.each do |g|
+          UsersGroup.all.each do |mb|
+                   if mb.user_id == session[:user_id] && mb.group_id == g.id
+                     @groups_belong_to << g
+                            unless Image.where(group_id: g.id).to_a.first == nil
+                                @images << Image.find(Image.where(group_id: g.id).to_a.first.id)
+                            end
+                   end
+           end
+      end
   end
 end
